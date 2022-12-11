2f073e1c95a87dcebdb2b170b5697f78f045a726
shfosem <ssh9806@naver.com>
2022-12-11 16:53:06 +0900

    1,2번 테스트 업로드

--- programmers/level1/1.py
+++ programmers/level1/1.py
@@ -1,8 +1,7 @@
-# 짝수와 홀수
-def solution(num):
-    answer = ''
-    if num % 2== 0:
-        answer+='Even'
-    else :
-        answer+='Odd'
+def solution(arr):
+    answer = 0
+    sum = 0
+    for num in arr:
+        sum+=num
+    answer = sum/len(arr)
     return answer
