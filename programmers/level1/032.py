5a03975f86b5bb8f5574baf543c1b6602fcdec5f
shfosem <ssh9806@naver.com>
2022-12-17 21:50:56 +0900

    32. 이상한 문자 만들기

--- programmers/level1/032.py
+++ programmers/level1/032.py
@@ -1,12 +1,15 @@
 # 32. 이상한 문자 만들기
-def solution(arr):
-    answer = []
-    answer.append(arr[0])
-    j=0
-    for i in range(1,len(arr)):
-        if answer[j] == arr[i]:
+def solution(s):
+    answer = ''
+    j = 0
+    for i in s:
+        if i == ' ':
+            answer+=i
+            j=0
             continue
-        answer.append(arr[i])
+        elif j%2==1:
+            answer+=i.lower()
+        else :
+            answer+=i.upper()
         j+=1
-            
     return answer
