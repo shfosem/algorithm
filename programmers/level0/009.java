dff688a6029885af99fbd6198cdaa27a06a83187
shfosem <ssh9806@naver.com>
2022-12-22 20:43:26 +0900

    9. 각도기

---
+++ programmers/level0/009.java
@@ -0,0 +1,17 @@
+// 9. 각도기
+class Solution {
+    public int solution(int angle) {
+        int answer = 0;
+        if(angle==90){
+            answer=2;
+        }else if(angle==180){
+            answer=4;
+        }else if(angle<90){
+            answer=1;
+        }else{
+            answer=3;
+        }
+        
+        return answer;
+    }
+}
