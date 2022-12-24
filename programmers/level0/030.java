b1f0bd8f1b3e752aa0bd5c1a5f2f57f61f981be6
shfosem <ssh9806@naver.com>
2022-12-24 12:39:43 +0900

    30.문자 반복 출력하기

--- programmers/level0/030.java
+++ programmers/level0/030.java
@@ -1,1 +1,13 @@
 // 30. 문자 반복 출력하기
+class Solution {
+    public String solution(String my_string, int n) {
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<my_string.length();i++){
+            //sb.append((my_string.charAt(i)+"").repeat(n));
+            for(int j=0; j<n; j++){
+                sb.append(my_string.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+}
