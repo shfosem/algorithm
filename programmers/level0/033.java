ead7630db2e7e88877be75843b273a103f100096
shfosem <ssh9806@naver.com>
2022-12-24 12:46:41 +0900

    33. 순서쌍의 개수

--- programmers/level0/033.java
+++ programmers/level0/033.java
@@ -1,11 +1,13 @@
 // 33. 순서쌍의 개수
+import java.util.stream.*;
 class Solution {
     public int solution(int n) {
-        int answer = 0;
+        return (int)IntStream.rangeClosed(1,n).filter(i -> n%i==0).count();
+        /*int answer = 0;
         for(int i=1; i<=n; i++){
             if(n%i==0)
                 answer+=1;
         }
-        return answer;
+        return answer;*/
     }
 }
