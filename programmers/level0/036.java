1e4c33a3c936b9c7a54818450010eeb7b85cbdd2
shfosem <ssh9806@naver.com>
2022-12-25 14:27:50 +0900

    36. 자릿수 더하기

---
+++ programmers/level0/036.java
@@ -0,0 +1,11 @@
+// 36. 자릿수 더하기
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        while(n>0){
+            answer+=n%10;
+            n/=10;
+        }
+        return answer;
+    }
+}
