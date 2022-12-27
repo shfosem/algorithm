80245209ce55a37fc47a0f96e11ab70c398391ad
shfosem <ssh9806@naver.com>
2022-12-27 21:24:47 +0900

    59. 합성수 찾기

---
+++ programmers/level0/059.java
@@ -0,0 +1,15 @@
+// 59. 합성수 찾기
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i=4; i<=n;i++){
+            for(int j=2; j<Math.sqrt(i)+1; j++){
+                if(i%j==0){
+                    answer+=1;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
