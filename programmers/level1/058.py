f10f93481bae553db5894f5213d3a9a08e6e1b36
shfosem <ssh9806@naver.com>
2023-01-02 19:43:05 +0900

    크레인 인형뽑기 게임

---
+++ programmers/level1/058.py
@@ -0,0 +1,22 @@
+# 58. 크레인 인형뽑기 게임
+def solution(board, moves):
+    n=len(board)
+    answer = 0
+    basket=[]
+    for idx in moves:
+        idx=idx-1
+        for i in range(n):
+            if board[i][idx]==0:
+                continue
+            else:
+                if not basket: # 기존 if len(basket)==0
+                    basket.append(board[i][idx])
+                else: 
+                    if basket[-1]==board[i][idx]: #기존 basket[len(bassket)-1]
+                        basket.pop()
+                        answer+=2
+                    else:
+                        basket.append(board[i][idx]) 
+                board[i][idx]=0
+                break
+    return answer
