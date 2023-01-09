e73224fecffa0f396529e851b4cd54cfd5a4c1cd
shfosem <ssh9806@naver.com>
2023-01-09 23:42:48 +0900

    15. 점프와 순간이동

--- programmers/level2/015.java
+++ programmers/level2/015.java
@@ -1,1 +1,18 @@
 // 15. 점프와 순간이동
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int ans = 0;
+        while(n>0){
+            if(n%2==0)
+                n/=2;
+            else{
+                n-=1;
+                ans+=1;
+            }
+        }
+
+        return ans;
+    }
+}
