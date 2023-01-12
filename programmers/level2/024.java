56e3661d7e1deaaf9e8ef18b9712116aeb051822
shfosem <ssh9806@naver.com>
2023-01-12 23:07:08 +0900

    n^2 배열 자르기

---
+++ programmers/level2/024.java
@@ -0,0 +1,20 @@
+// 24. n^2 배열 자르기
+class Solution {
+    public int[] solution(int n, long left, long right) {
+        final int LEN = (int)(right - left) + 1;
+        int[] answer = new int[LEN];
+        
+        for(int i = 0; i< LEN; i++){
+            answer[i]= getNum((long)i+left, n);
+        }
+        
+        return answer;
+    }
+    int getNum(long idx, int n){
+        if(idx%n < idx/n)
+            return (int)(idx/n)+1;
+        else
+            return (int)(idx%n)+1;
+            
+    }
+}
