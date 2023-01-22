22f42c2e7bb2bd35f064131748caf822bb81be8e
shfosem <ssh9806@naver.com>
2023-01-22 22:58:48 +0900

    44. 모음사전

---
+++ programmers/level2/044.java
@@ -0,0 +1,24 @@
+// 44. 모음사전
+import java.util.*;
+class Solution {
+    static ArrayList<String> arr = new ArrayList<>();
+    static char[] ch = {'A', 'E', 'I', 'O', 'U'};
+    public int solution(String word) {
+        int answer = 0;
+        StringBuilder sb = new StringBuilder();
+        insert(sb,0);
+        return arr.indexOf(word);
+    }
+    void insert(StringBuilder sb, int len){
+        arr.add(sb.toString());
+        if(len==5){
+            return;
+        }else{
+            for(int i=0; i<5; i++){
+                insert(sb.append(ch[i]),len+1);
+                sb.deleteCharAt(sb.length()-1);                
+            }
+        }
+        
+    }
+}
