d7d3a892ee995bf4a2a1495dda50d37f1a9dc55e
shfosem <ssh9806@naver.com>
2023-02-19 11:47:04 +0900

    53. 큰 수 만들기

--- programmers/level2/053.java
+++ programmers/level2/053.java
@@ -41,3 +41,26 @@ class Solution {
         return;
     }
 }
+
+
+
+// 스택 활용 풀이
+import java.util.*;
+class Solution {
+    public String solution(String number, int k) {
+        char[] result = new char[number.length() - k];
+        Stack<Character> stack = new Stack<>();
+
+        for (int i=0; i<number.length(); i++) {
+            char c = number.charAt(i);
+            while (!stack.isEmpty() && stack.peek() < c && k-- > 0) {
+                stack.pop();
+            }
+            stack.push(c);
+        }
+        for (int i=0; i<result.length; i++) {
+            result[i] = stack.get(i);
+        }
+        return new String(result);
+    }
+}
